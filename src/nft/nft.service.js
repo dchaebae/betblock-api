f50fcb3466c50de343f55456e5185ddf798e7dba
dchaebae <danielkchae@gmail.com>
2023-12-07 16:14:36 -0500

    clean up for deployment

--- src/nft/nft.service.js
+++ src/nft/nft.service.js
@@ -2,15 +2,12 @@ import OpenAI from 'openai'
 import dotenv from 'dotenv'
 import {create} from 'ipfs-http-client'
 import fs from 'fs'
-// import FormData from 'form-data'
-// import axios from 'axios'
 
 dotenv.config();
 
 const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY})
 const ipfs = create({url: 'http://localhost:5001/api/v0'})
 var env = process.env.NODE_ENV || 'development'
-//var JWT = process.env.PINATA_API_KEY
 
 export const invokeDallE = async (words) => {
 	const image = await openai.images.generate({
@@ -25,37 +22,11 @@ export const invokeDallE = async (words) => {
 }
 
 export const addImageToIPFS = async (binaryData) => {
-// 	const formData = new FormData();
-//   formData.append('file', binaryData, 'image.png')
-//   
-//   const pinataMetadata = JSON.stringify({
-//     name: 'Generated Image',
-//   });
-//   formData.append('pinataMetadata', pinataMetadata);
-//   
-//   const pinataOptions = JSON.stringify({
-//     cidVersion: 0,
-//   })
-//   formData.append('pinataOptions', pinataOptions);
-// 
-//   try{
-//     const res = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
-//       maxBodyLength: "Infinity",
-//       headers: {
-//         'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
-//         'Authorization': `Bearer ${JWT}`
-//       }
-//     });
-//     console.log(res.data);
-//   } catch (error) {
-//     console.log(error);
-//   }
 	try {
 		const result = await ipfs.add(binaryData);
 		const ipfsHash = result.cid.toString();
 		console.log('Image added to IPFS. IPFS Hash: ', ipfsHash);
-		await ipfs.pin.add(ipfsHash); // explicitly pin with my ipfs
-		return '';
+		return ipfsHash;
 	}
 	catch (error) {
 		console.error('Error adding image to IPFS: ', error.message);
