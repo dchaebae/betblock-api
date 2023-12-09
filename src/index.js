c59890ed9c7d0ffcdd7273a7045dffb7efe7f138
dchaebae <danielkchae@gmail.com>
2023-12-09 01:52:47 -0500

    ssl for aws

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import nftController from "./nft/nft.controller.js";
 import dotenv from 'dotenv'
+import https from 'https'
+import fs from 'fs'
 
 dotenv.config();
 
@@ -14,6 +16,14 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`[server]: ⚡️ Server is running at http://localhost:${port}`);
 });
+
+if (process.env.NODE_ENV === 'production') {
+  const options = {
+    cert: fs.readFileSync(process.env.PATH_SSL_CERTIFICATE),
+    key: fs.readFileSync(process.env.PATH_SSL_KEY)
+  }
+  https.createServer(options, app).listen(443)
+}
 
 // Setup routes
 app.use(nftController);
