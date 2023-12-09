c387d9531da1ba45d6fe3982165937c810c6a03a
dchaebae <danielkchae@gmail.com>
2023-12-08 22:24:31 -0500

    add apikey and a tester

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Betblock API");
+  res.send("Welcome to Betblock API!");
 });
 
 app.listen(port, () => {
--- src/nft/nft.controller.js
+++ src/nft/nft.controller.js
@@ -3,25 +3,41 @@ import {
   invokeDallE,
   addImageToIPFS
 } from './nft.service.js'
-import { testImg } from './testimage.js'
+// import { testImg } from './testimage.js'
 import dotenv from 'dotenv'
 
 dotenv.config();
 
 const nftController = Router();
 
-nftController.get("/generateImage", async (req, res, next) => {
+const apiKeyMiddleware = (req, res, next) => {
+  const apiKey = req.headers['x-api-key']
+
+  // prevent anyone from just calling our very public API :)
+  if (!apiKey || apiKey !== process.env.INTERNAL_API_KEY) {
+    res.status(401).json({error: 'Need API Key'});
+    return;
+  }
+
+  next();
+}
+
+// invoke DALLE to make NFT image!
+nftController.get("/generateImage", apiKeyMiddleware, async (req, res, next) => {
   const words = req.query.words;
-  let output = await invokeDallE(words)
-  //let output = testImg
+  // let output = await invokeDallE(words)
+  let output = testImg
   let binaryData = Buffer.from(output, 'base64');
   await addImageToIPFS(binaryData).then((response) => {
     res.send({words: words, environment: process.env.NODE_ENV, data: response});
   }).catch((error) => {
     res.status(500).send(error.message);  
   })
+});
 
-  next()
+// validate the key, primarily for testing purposes
+nftController.get("/validateKey", apiKeyMiddleware, async (req, res, next) => {
+  res.send('This key is valid!')
 });
 
 export default nftController;
