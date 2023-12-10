4e4aae1fbd7ee638e1a830ae6cfd187e4a3b1540
dchaebae <danielkchae@gmail.com>
2023-12-10 16:47:25 -0500

    update controller to be prod-ready

--- src/nft/nft.controller.js
+++ src/nft/nft.controller.js
@@ -14,10 +14,10 @@ const nftController = Router();
 const apiKeyMiddleware = (req, res, next) => {
   const apiKey = req.headers['x-api-key']
   // prevent anyone from just calling our very public API :)
-  // if (!apiKey || apiKey !== process.env.INTERNAL_API_KEY) {
-  //   res.status(401).json({error: 'Need API Key'});
-  //   return;
-  // }
+  if (!apiKey || apiKey !== process.env.INTERNAL_API_KEY) {
+    res.status(401).json({error: 'Need API Key'});
+    return;
+  }
 
   next();
 }
@@ -26,33 +26,32 @@ const apiKeyMiddleware = (req, res, next) => {
 nftController.get("/generateImage", apiKeyMiddleware, async (req, res, next) => {
   const words = req.query.words;
   const tokenId = req.query?.tokenId;
-  let sampleOutput = {
-      cid: 'QmQ8kVmrQgSy9VY7jfNqHwrkdbHeqZUQg6EUGFGSJ6eLEQ',
-      name: 'betblock bio 0',
-      description: 'betblock bio - newbie level: excited puppy jumping up and down',
-      image: 'ipfs://QmQjGcrVEdfTvin4uH45xwKEQPL2j8WJU7AfCyTkUoWmTB'
-    };
-  res.end(JSON.stringify(sampleOutput))
-//   let output = await invokeDallE(words).catch((error) => {
-//     res.status(400).json({error: error.message})
-//     return
-//   })
-// 
-//   // let output = testImg
-//   let binaryData = Buffer.from(output, 'base64');
-//   const imageCid = await addImageToIPFS(binaryData).catch((error) => {
-//     res.status(500).json({error: error.message});
-//     return
-//   })
-// 
-//   await addMetadataToIPFS(imageCid, tokenId, words).then((response) => {
-//     console.log(response)
-//     res.send(response);
-//     return
-//   }).catch(error => {
-//     res.status(500).json({error: error.message})
-//     return
-//   })
+  // let sampleOutput = {
+  //     cid: 'QmQ8kVmrQgSy9VY7jfNqHwrkdbHeqZUQg6EUGFGSJ6eLEQ',
+  //     name: 'betblock bio 0',
+  //     description: 'betblock bio - newbie level: excited puppy jumping up and down',
+  //     image: 'ipfs://QmQjGcrVEdfTvin4uH45xwKEQPL2j8WJU7AfCyTkUoWmTB'
+  //   };
+  // res.end(JSON.stringify(sampleOutput))
+  let output = await invokeDallE(words).catch((error) => {
+    res.status(400).json({error: error.message})
+    return
+  })
+
+  // let output = testImg
+  let binaryData = Buffer.from(output, 'base64');
+  const imageCid = await addImageToIPFS(binaryData).catch((error) => {
+    res.status(500).json({error: error.message});
+    return
+  })
+
+  await addMetadataToIPFS(imageCid, tokenId, words).then((response) => {
+    res.end(JSON.stringify(response));
+    return
+  }).catch(error => {
+    res.status(500).json({error: error.message})
+    return
+  })
 });
 
 // validate the key, primarily for testing purposes
