31e023806a605de92ba72b6c550adccc826d06d1
dchaebae <danielkchae@gmail.com>
2023-12-10 17:04:58 -0500

    verify logging, response does not have data

--- src/nft/nft.controller.js
+++ src/nft/nft.controller.js
@@ -46,8 +46,7 @@ nftController.get("/generateImage", apiKeyMiddleware, async (req, res, next) => 
   })
 
   await addMetadataToIPFS(imageCid, tokenId, words).then((response) => {
-    console.log(response)
-    res.end(JSON.stringify(response.data));
+    res.end(JSON.stringify(response));
     return
   }).catch(error => {
     res.status(500).json({error: error.message})
