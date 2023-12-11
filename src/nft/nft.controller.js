b9db2eeab122b09286485cecfe325a760088ae23
dchaebae <danielkchae@gmail.com>
2023-12-10 22:47:07 -0500

    controller and service updated for final submission;

--- src/nft/nft.controller.js
+++ src/nft/nft.controller.js
@@ -28,33 +28,33 @@ nftController.get("/generateImage", apiKeyMiddleware, async (req, res, next) => 
   const words = req.query.words;
   const tokenId = req.query?.tokenId;
 
-  let imageCID = CID.parse('QmQ8kVmrQgSy9VY7jfNqHwrkdbHeqZUQg6EUGFGSJ6eLEQ')
-  let sampleOutput = {
-      // cid: 'QmQ8kVmrQgSy9VY7jfNqHwrkdbHeqZUQg6EUGFGSJ6eLEQ',
-      // name: 'betblock bio 0',
-      // description: 'betblock bio - newbie level: excited puppy jumping up and down',
-      image: imageCID.toV1().toString()
-    };
+  // let imageCID = CID.parse('QmQ8kVmrQgSy9VY7jfNqHwrkdbHeqZUQg6EUGFGSJ6eLEQ')
+  // let sampleOutput = {
+  //     // cid: 'QmQ8kVmrQgSy9VY7jfNqHwrkdbHeqZUQg6EUGFGSJ6eLEQ',
+  //     // name: 'betblock bio 0',
+  //     // description: 'betblock bio - newbie level: excited puppy jumping up and down',
+  //     image: imageCID.toV1().toString()
+  //   };
   res.end(JSON.stringify(sampleOutput))
-//   let output = await invokeDallE(words).catch((error) => {
-//     res.status(400).json({error: error.message})
-//     return
-//   })
-// 
-//   // let output = testImg
-//   let binaryData = Buffer.from(output, 'base64');
-//   const imageCid = await addImageToIPFS(binaryData).catch((error) => {
-//     res.status(500).json({error: error.message});
-//     return
-//   })
-// 
-//   await addMetadataToIPFS(imageCid, tokenId, words).then((response) => {
-//     res.end(response.image);
-//     return
-//   }).catch(error => {
-//     res.status(500).json({error: error.message})
-//     return
-//   })
+  let output = await invokeDallE(words).catch((error) => {
+    res.status(400).json({error: error.message})
+    return
+  })
+
+  // let output = testImg
+  let binaryData = Buffer.from(output, 'base64');
+  const imageCid = await addImageToIPFS(binaryData).catch((error) => {
+    res.status(500).json({error: error.message});
+    return
+  })
+
+  await addMetadataToIPFS(imageCid, tokenId, words).then((response) => {
+    res.end(response);
+    return
+  }).catch(error => {
+    res.status(500).json({error: error.message})
+    return
+  })
 });
 
 // validate the key, primarily for testing purposes
